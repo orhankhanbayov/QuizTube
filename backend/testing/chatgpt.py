4dd20515393f2a8af7d463c20ab29a914aa16bd5
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-03-30 21:40:10 +0100

    using captions instead of whisper

--- backend/testing/chatgpt.py
+++ backend/testing/chatgpt.py
@@ -1,22 +1,25 @@
 import openai
 import re
 import json
-from youtube_transcript_api import YouTubeTranscriptApi
-openai.api_key = os.getenv('API_KEY')
+import os
 
-a = YouTubeTranscriptApi.get_transcript("ORMx45xqWkA")
-texts = []
-for obj in a:
-    texts.append(obj['text'])
-text = ''.join(texts)
+from youtube_transcript_api import YouTubeTranscriptApi
+openai.api_key = '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'
+
+# a = YouTubeTranscriptApi.get_transcript("ORMx45xqWkA")
+# texts = []
+# for obj in a:
+#     texts.append(obj['text'])
+# text = ''.join(texts)
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=(f"Generate 5 multiple choice questions based on the following text and return with answers in an array of json objects, one for each questions containing the question, choices and answer:{text} "),
+  prompt=(f"Generate 5 multiple choice questions"),
   max_tokens=3000,
   temperature=0
 )
-string_data = response["choices"][0].text
-python_data = json.loads(string_data)
-print(python_data)
+# string_data = response["choices"][0].text
+# python_data = json.loads(string_data)
+# print(python_data)
 
 
+print(response)
